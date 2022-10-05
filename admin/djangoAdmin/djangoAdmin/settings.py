9c0cb9a4977d9afac5716bdc46a04092b07faa5f
Roninore L <roninore@github.com>
2022-10-05 21:25:11 +0300

    change requirements

--- admin/djangoAdmin/djangoAdmin/settings.py
+++ admin/djangoAdmin/djangoAdmin/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = CONFIG_FILE['djangoKey']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ['NODE_ENV'] == 'dev' else False 
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['1.1.1.1', '127.0.0.1','localhost']
 
 
 # Application definition
