3baa1dc29cf422e4482cbf0ce850cff8e4242956
Roninore L <roninore@github.com>
2022-10-05 23:46:53 +0300

    settings

--- admin/djangoAdmin/djangoAdmin/settings.py
+++ admin/djangoAdmin/djangoAdmin/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = CONFIG_FILE['djangoKey']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False 
 
-ALLOWED_HOSTS = ['1.1.1.1', '127.0.0.1','localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
