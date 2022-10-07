921f5942d1ec9f760ba509af120373f29cbe12ee
Roninore L <roninore@github.com>
2022-10-07 19:51:59 +0300

    add handling error on start polling

--- ClassBot/methods/startPolling.js
+++ ClassBot/methods/startPolling.js
@@ -1,9 +1,17 @@
 
 module.exports = async function() {
-  const {response} = await this.apiRequest('groups.getLongPollServer', {group_id:this.group_id})
-  // console.log(response)
-  
-  const lp_url = `${response.server}?act=a_check&key=${response.key}&ts=${response.ts}&wait=25`
-  console.log('LongPoll url: ', lp_url)
-  await this.polling(response.server,response.key,response.ts)
+  async function startPollingIn() {
+    const {response} = await this.apiRequest('groups.getLongPollServer', {group_id:this.group_id})
+    // console.log(response)
+    
+    const lp_url = `${response.server}?act=a_check&key=${response.key}&ts=${response.ts}&wait=25`
+    console.log('LongPoll url: ', lp_url)
+    await this.polling(response.server,response.key,response.ts)
+  }
+  try {
+    startPollingIn.call(this)
+  } catch(e) {
+    console.log('Error on start polling', e)
+    setTimeout(this.startPolling,5000)
+  }
   }
