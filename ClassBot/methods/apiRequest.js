a6bbb3ce59c3cb8e06e3371655a3e2ae28fc99c1
Roninore L <roninore@github.com>
2022-10-14 16:15:20 +0300

    fix crashes on apiRequest

--- ClassBot/methods/apiRequest.js
+++ ClassBot/methods/apiRequest.js
@@ -2,6 +2,7 @@ const axios = require('axios').default
 const { stringify } = require('querystring');
 
 module.exports = async function(method, params = {}) {
+  try {
     const { data } = await axios.post(`https://api.vk.com/method/${method}/`, stringify({
         access_token: this.access_token,
         v: this.lp_version,
@@ -13,4 +14,7 @@ module.exports = async function(method, params = {}) {
     }
   
     return data
+  } catch(e) {
+    return {error:e}
+  }
   }
